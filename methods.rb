74c7bcbc60d3b6acf20e955f353c2efedc57dcb5
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-01-12 13:50:09 -0300

    remove comments

--- methods.rb
+++ methods.rb
@@ -1,27 +1,30 @@
 require 'date'
-# def full_name(first_name, last_name)
-#   name = first_name.capitalize + " " + last_name.capitalize
-#   return name
-# end
 
-# puts full_name("boris", "paillard")   # => "Boris Paillard"
-# puts full_name("seb", "saunier")
+def full_name(first_name, last_name)
+  name = first_name.capitalize + " " + last_name.capitalize
+  return name
+end
 
-# def max(number, count)
-#   if number.size != count
-#     puts "CPF Inválido"
-#   else
-#     puts "CPF Válido"
-#   end
-# end
+puts full_name("boris", "paillard")   # => "Boris Paillard"
+puts full_name("seb", "saunier")
 
-# digits = 11
-# cpf = "44444444444"
-# puts max(cpf, digits)
+def max(number, count)
+  if number.size != count
+    puts "CPF Inválido"
+  else
+    puts "CPF Válido"
+  end
+end
+
+digits = 11
+cpf = "44444444444"
+puts max(cpf, digits)
 
 def tomorrow
   tomorrow_date = Date.today + 1
   return tomorrow_date.strftime("%B %d")
 end
+
+
 
 puts tomorrow   # => "October 4"
